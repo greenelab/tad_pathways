8ac411921e13855bc27dab44e07bad8cfd0feff3
Greg Way <gregory.way@gmail.com>
2016-06-10 09:04:04 -0400

    reverting NHGRI-EBI_GWAS_summary.R

--- NHGRI-EBI_GWAS_summary.R
+++ NHGRI-EBI_GWAS_summary.R
@@ -7,8 +7,7 @@
 # required journals)
 
 # Usage:
-# This script is run by 'ANALYSIS.sh' but can also be run directly by calling
-# from the parent directory 'R --no-save < NHGRI-EBI_GWAS_summary.R'
+# This script is run by 'ANALYSIS.sh'
 
 # Output:
 # Several chromosome specific tables and a large summary table of all findings
@@ -28,15 +27,13 @@ num_snps <- c()
 for (trait in traits) {
   gwas_subset <- gwas_filter[gwas_filter$`DISEASE/TRAIT` == trait, ]
   num_snps <- c(num_snps, nrow(gwas_subset))
-  cat(trait, ' ', nrow(gwas_subset), '\n')
   gwas_traitinfo[[trait]] <- gwas_subset[ , c('SNPS', 'CHR_ID', 'CHR_POS', 
                                               'DISEASE/TRAIT', 
                                               'REPORTED GENE(S)', 'MAPPED_GENE',
                                               'PUBMEDID')]
   trait_name <- gsub(' ', '_', trait)
   trait_name <- gsub('[/:(),.-]', '_', trait_name)
-  filename = paste0('data/gwas_catalog/', trait_name, '.tsv')
-  print(filename)
+  filename <- paste0('data/gwas_catalog/', trait_name, '_hg19.tsv')
   readr::write_tsv(gwas_traitinfo[[trait]], filename)
 }
 
@@ -49,34 +46,3 @@ hist(num_snps_trait, breaks = 100, xlab = 'Number of SNPs',
      main = 'NHGRI-EBI GWAS\nNumber of Significant SNPs per Disease/Trait',
      cex = 5)
 dev.off()
-
-# Save the total SNP information to a file separated by chromosome
-gwas_use_data <- gwas_filter[, c('SNPS', 'CHR_ID', 'CHR_POS', 'DISEASE/TRAIT',
-                                 'REPORTED GENE(S)', 'MAPPED_GENE', 'PUBMEDID')]
-
-gwas_use_data <- gwas_use_data[gwas_use_data$SNPS != '', ]
-gwas_use_data_chrom <- list()
-for (chrom in 1:length(unique(gwas_use_data$CHR_ID))){
-  gwas_use_data_chrom[[chrom]] <- gwas_use_data[gwas_use_data$CHR_ID == chrom, ]
-  write.table(gwas_use_data_chrom[[chrom]], 
-              paste0('data/SNAP/all_unique_snps_chr', chrom, '.tsv'), 
-              col.names = F, row.names = F, quote = F, sep = '\t')
-}
-
-# Save to file with chromosome information
-gwas_snps_unique_ch <- cbind(gwas_use_data$SNPS, gwas_use_data$CHR_ID)
-write.table(gwas_snps_unique_ch, 'data/SNAP/all_unique_trait_snps_chrID.csv', 
-            sep = ',', col.names = F, row.names = F, quote = F)
-
-# Chromosome specific files are input into:
-# https://www.broadinstitute.org/mpg/snap/ldsearchpw.php
-# Parameters: 
-# SNP data set: 1000 Genomes Pilot 1
-# r2 threshold: No limit
-# Population panel: CEU
-# Distance limit: 500
-# Download to: File
-# Include each query snp as a proxy for itself: yes
-# Suppress warning messages in output: no
-# Filter by Array: No
-# Output Columns: D', Genome Coordinates
